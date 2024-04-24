812edf05dc48d20f668ff7bf99c5347381fb5977
zahra <mousavizahra681@gmail.com>
2024-04-24 17:19:56 +0330

    my fourth commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: ' ',
-          port: '',
-          pathname: '',
-        },
-      ],
-    },
-  }
+// module.exports = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: ' ',
+//           port: '',
+//           pathname: '',
+//         },
+//       ],
+//     },
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
